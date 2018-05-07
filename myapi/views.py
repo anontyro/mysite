5166c73e514f3e71d3b5d417852678e600427f64
Alexander Wilkinson <alexwilkinson@gmail.com>
2018-05-07 22:24:21 +0800

    reviewing API section

--- myapi/views.py
+++ myapi/views.py
@@ -3,6 +3,7 @@ from blog import models as bModels
 from portfolio import models as pModels
 from myapi import serializers
 from rest_framework import viewsets, permissions
+from django.shortcuts import get_object_or_404
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -12,6 +13,20 @@ class PostViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = bModels.Post.objects.active().order_by("-publish")
     serializer_class = serializers.PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class PostNewViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        queryset = bModels.Post.objects.active().order_by("-publish")
+        serializer_class = serializers.PostSerializer(queryset, many=True)
+        return Response(serializer_class.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = bModels.Post.objects.all()
+        post = get_object_or_404(queryset, pk=pk)
+        serializer_class = serializers.PostSerializer(post)
+        return Response(serializer_class.data)
 
 class PortfolioViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = pModels.Portfolio.objects.active().order_by("-publish")
