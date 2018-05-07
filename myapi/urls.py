2737d89b7ee7bb735547a4f900b79dca2797a3c1
Alexander Wilkinson <alexwilkinson@gmail.com>
2018-05-07 22:47:04 +0800

    trying out different methods for the api, for some reason using generics has lost the endpoint eventhouhg it is registered in the url section

--- myapi/urls.py
+++ myapi/urls.py
@@ -10,6 +10,6 @@ app_name='myapi'
 router = routers.SimpleRouter()
 router.register(r'posts',views.PostViewSet)
 router.register(r'portfolio', views.PortfolioViewSet)
-router.register(r'postnew',views.PostNewViewSet, 'postnew')
+router.register(r'postnew',views.PostNewViewSet.as_view(), 'postnew')
 
 urlpatterns = router.urls
